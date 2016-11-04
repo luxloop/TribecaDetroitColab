48c730366a206b196b84a5aa898e284adc95f137
Ivaylo Getov <contact@ivaylogetov.com>
2016-11-04 11:59:01 -0400

    complete proof of concept

--- TribecaDetroitColab/TribecaDetroitColab/ViewController.swift
+++ TribecaDetroitColab/TribecaDetroitColab/ViewController.swift
@@ -7,12 +7,59 @@
 //
 
 import UIKit
+import AudioKit
 
 class ViewController: UIViewController {
+  
+  @IBOutlet weak var colorBox: UIView!
+  
+  var mic: AKMicrophone!
+  var tracker: AKFrequencyTracker!
+  var silence: AKBooster!
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    AKSettings.audioInputEnabled = true
+    mic = AKMicrophone()
+    tracker = AKFrequencyTracker.init(mic, hopSize:128, peakCount:100)
+    silence = AKBooster(tracker, gain: 0)
+  }
+  
+  override func viewDidAppear(_ animated: Bool) {
+    super.viewDidAppear(animated)
+    
+    AudioKit.output = silence
+    AudioKit.start()
+    
+    Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(ViewController.update), userInfo: nil, repeats: true)
+  }
+  
+  
+  func update() {
+    //print(tracker.frequency)
+    //if tracker.amplitude > 0.1 {
+    if tracker.frequency > 12000 {
+      //frequencyLabel.text = String(format: "%0.1f", tracker.frequency)
+      print(tracker.frequency)
+    
+      if tracker.frequency > 17900 {
+        colorBox.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1)
+      } else if tracker.frequency > 17400 {
+        colorBox.backgroundColor = UIColor(red: 0, green: 1, blue: 0, alpha: 1)
+      } else if tracker.frequency > 16900 {
+        colorBox.backgroundColor = UIColor(red: 0, green: 0, blue: 1, alpha: 1)
+      } else if tracker.frequency > 16400 {
+        colorBox.backgroundColor = UIColor(red: 1, green: 1, blue: 0, alpha: 1)
+      } else if tracker.frequency > 15900 {
+        colorBox.backgroundColor = UIColor(red: 0, green: 1, blue: 1, alpha: 1)
+      } else if tracker.frequency > 15400 {
+        colorBox.backgroundColor = UIColor(red: 1, green: 0, blue: 1, alpha: 1)
+      } else if tracker.frequency > 14900 {
+        colorBox.backgroundColor = UIColor(red: 1, green: 0, blue: 0, alpha: 1)
+      }
+      
+    }
   }
 
   override func didReceiveMemoryWarning() {
