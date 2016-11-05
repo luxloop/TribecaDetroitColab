7731b2b4d023316394666a6effc959b851afccc1
Ivaylo Getov <contact@ivaylogetov.com>
2016-11-05 18:19:25 -0400

    cleanup project

--- TribecaDetroitColab/TribecaDetroitColab/ViewController.swift
+++ TribecaDetroitColab/TribecaDetroitColab/ViewController.swift
@@ -15,29 +15,29 @@ class ViewController: UIViewController {
   
   @IBOutlet weak var colorBox: UIView!
   @IBOutlet weak var vidContainer: UIView!
+  @IBOutlet weak var backgroundVidContainer: UIView!
   
   var mic: AKMicrophone!
   var tracker: AKFrequencyTracker!
   var silence: AKBooster!
-//  var fft: AKFFTTap!
 
   var movPlayer:AVPlayer?
+  var backgroundMovie:AVPlayer?
   
   let codeWindow = 5.0
   let updateInterval = 0.05
   var maxCount:Int = 0;
   var listenForCode = false
-  //var listenTimer:Timer?
   
   var counter:Int = 0
   let frequencyRange = 90.0
   
   let startSignal:Double = 17500
   let signalOptions:[(freq:Double,file:String)] =
-    [(16750,"plantTest"),
-     (16500,"plantTest"),
-     (16250,"plantTest"),
-     (16000,"gwcTest")]
+    [(16000,"PBD_Chapter1"),
+     (16500,"PBD_Chapter2"),
+     (16250,"PBD_Chapter3"),
+     (16750,"PBD_Chapter4")]
   
   func changeAudioMode() {
     do {
@@ -59,21 +59,21 @@ class ViewController: UIViewController {
     
     changeAudioMode()
     maxCount = Int((1/updateInterval) * codeWindow)
-    //print("maxcount: \(maxCount)")
     
     AKSettings.audioInputEnabled = true
     AKSettings.sampleRate = 96000
     mic = AKMicrophone()
     tracker = AKFrequencyTracker.init(mic, hopSize:64, peakCount:4)
     silence = AKBooster(tracker, gain: 0)
-//    fft = AKFFTTap(mic)
-    
     
   }
   
   override func viewDidAppear(_ animated: Bool) {
     super.viewDidAppear(animated)
     
+    NotificationCenter.default.addObserver(self, selector: #selector(self.playerDidFinishPlaying), name: NSNotification.Name.AVPlayerItemDidPlayToEndTime, object: nil)
+    
+    setupBackground()
     setupVideo()
     
     AudioKit.output = silence
@@ -95,8 +95,6 @@ class ViewController: UIViewController {
   
   func resetListenState() {
     listenForCode = false
-    //listenTimer?.invalidate()
-    //listenTimer = nil;
     counter = 0
     print("Stop Listen")
   }
@@ -116,9 +114,6 @@ class ViewController: UIViewController {
         print("Start Listen")
         listenForCode = true;
         counter = 0
-        //listenTimer?.invalidate()
-        //listenTimer = Timer.scheduledTimer(timeInterval: codeWindow, target: self, selector: #selector(ViewController.resetListenState), userInfo: nil, repeats: false)
-        
       } else if listenForCode {
         for option in signalOptions {
           if isMatch(input: tracker.frequency, reference: option.freq) {
@@ -135,37 +130,28 @@ class ViewController: UIViewController {
             break
           }
         }
-        
       }
-      
-      
-      
-//      //frequencyLabel.text = String(format: "%0.1f", tracker.frequency)
-//      print(tracker.frequency)
-//    
-//      if tracker.frequency > 17900 {
-//        colorBox.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1)
-//      } else if tracker.frequency > 17400 {
-//        colorBox.backgroundColor = UIColor(red: 0, green: 1, blue: 0, alpha: 1)
-//      } else if tracker.frequency > 16900 {
-//        colorBox.backgroundColor = UIColor(red: 0, green: 0, blue: 1, alpha: 1)
-//        //setupVideo()
-//      } else if tracker.frequency > 16400 {
-//        colorBox.backgroundColor = UIColor(red: 1, green: 1, blue: 0, alpha: 1)
-//      } else if tracker.frequency > 15900 {
-//        colorBox.backgroundColor = UIColor(red: 0, green: 1, blue: 1, alpha: 1)
-//      } else if tracker.frequency > 15400 {
-//        colorBox.backgroundColor = UIColor(red: 1, green: 0, blue: 1, alpha: 1)
-//      } else if tracker.frequency > 14900 {
-//        colorBox.backgroundColor = UIColor(red: 1, green: 0, blue: 0, alpha: 1)
-//      }
     }
+  }
+  
+  func setupBackground() {
+    
+    let pathToFile = Bundle.main.path(forResource: "Videos/ScreenBackground", ofType: "mp4")
+    let pathURL = NSURL.fileURL(withPath: pathToFile!)
+
+    backgroundMovie = AVPlayer(url:pathURL)
+    let movLayer = AVPlayerLayer(player: backgroundMovie)
+    backgroundVidContainer.layer.addSublayer(movLayer)
+    movLayer.frame = backgroundVidContainer.bounds
+    movLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+    backgroundMovie?.volume = 0;
+    backgroundMovie?.play()
   }
   
   func setupVideo() {
     
-    let pathToEx1 = Bundle.main.path(forResource: "Videos/plantTest", ofType: "mp4")
-    let pathURL = NSURL.fileURL(withPath: pathToEx1!)
+    let pathToFile = Bundle.main.path(forResource: "Videos/PBD_Chapter1", ofType: "mp4")
+    let pathURL = NSURL.fileURL(withPath: pathToFile!)
     
     movPlayer = AVPlayer(url:pathURL)
   
@@ -174,48 +160,40 @@ class ViewController: UIViewController {
     movLayer.frame = vidContainer.bounds
     
     movLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
-    
-    //NotificationCenter.default.addObserver(self, selector: "playerDidFinishPlaying:",name: NSNotification.Name.AVPlayerItemDidPlayToEndTime, object: movPlayer?.currentItem)
-    
-    //NotificationCenter.default.addObserver(forName: NSNotification.Name.AVPlayerItemDidPlayToEndTime, object: nil, queue: nil) { _ in playerDidFinishPlaying }
-    
-    NotificationCenter.default.addObserver(self, selector: #selector(self.playerDidFinishPlaying), name: NSNotification.Name.AVPlayerItemDidPlayToEndTime, object: nil)
-
-
     movPlayer?.volume = 0;
-    //movPlayer?.play()
     
     if let name = getCurrentVideoName() {
       print(name)
     }
     
     vidContainer.alpha = 0.0;
-    //playCurrentVideo()
-    //cueAndPlayVideo(fileName: "plantTest")
   }
   
   func playCurrentVideo() {
-    //vidContainer.alpha = 1.0;
     fadeVideoUp()
     movPlayer?.play()
-    //changeAudioMode()
   }
   
   func stopVideo() {
-    //vidContainer.alpha = 0.0;
     fadeVideoDown()
+//    let t1 = CMTimeMake(5, 100);
+//    self.movPlayer?.seek(to: t1)
     movPlayer?.pause()
-    //changeAudioMode()
   }
   
   func playerDidFinishPlaying(note: Notification) {
-    //print("Video Finished - \(note.object)")
     if let currentItem = note.object as? AVPlayerItem,
     let asset = currentItem.asset as? AVURLAsset {
       var filename = asset.url.lastPathComponent.characters.split{$0 == "."}.map(String.init)
-      print(filename[0]);
+      print("video ended: \(filename[0])");
+      if filename[0] == "ScreenBackground" {
+        let t1 = CMTimeMake(5, 100);
+        self.backgroundMovie!.seek(to: t1)
+        self.backgroundMovie!.play()
+      } else {
+        stopVideo()
+      }
     }
-    stopVideo()
   }
   
   func getCurrentVideoName() -> String? {
@@ -227,19 +205,15 @@ class ViewController: UIViewController {
   }
   
   func cueAndPlayVideo(fileName: String) {
-    //changeAudioMode()
     let pathToEx1 = Bundle.main.path(forResource: "Videos/\(fileName)", ofType: "mp4")
     let pathURL = NSURL.fileURL(withPath: pathToEx1!)
     let asset = AVAsset(url: pathURL)
-    //let assetKeys = [ "playable" ]
     let playerItem = AVPlayerItem(asset: asset)
     movPlayer?.pause()
     movPlayer?.replaceCurrentItem(with: playerItem)
     movPlayer?.volume = 0
-    //vidContainer.alpha = 1.0;
     fadeVideoUp()
     movPlayer?.play()
-    //AudioKit.start()
   }
   
   func fadeVideoDown() {
@@ -254,7 +228,5 @@ class ViewController: UIViewController {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-
-
 }
 
