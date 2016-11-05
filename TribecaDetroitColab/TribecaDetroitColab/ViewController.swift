880053b1d415cfce27a947156604132da08fcaaa
Ivaylo Getov <contact@ivaylogetov.com>
2016-11-05 09:55:36 -0400

    Change tone-listen logic to time out later

--- TribecaDetroitColab/TribecaDetroitColab/ViewController.swift
+++ TribecaDetroitColab/TribecaDetroitColab/ViewController.swift
@@ -20,18 +20,49 @@ class ViewController: UIViewController {
   var tracker: AKFrequencyTracker!
   var silence: AKBooster!
 //  var fft: AKFFTTap!
-  
-  //var moviePlayer:MPMoviePlayerController?
 
   var movPlayer:AVPlayer?
+  
+  let codeWindow = 5.0
+  let updateInterval = 0.05
+  var maxCount:Int = 0;
+  var listenForCode = false
+  //var listenTimer:Timer?
+  
+  var counter:Int = 0
+  let frequencyRange = 90.0
+  
+  let startSignal:Double = 17500
+  let signalOptions:[(freq:Double,file:String)] =
+    [(17300,"plantTest"),
+     (17100,"gwcTest")]
+  
+  func changeAudioMode() {
+    do {
+      try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayAndRecord)
+    } catch let error as NSError {
+      print(error)
+    }
+    
+    do {
+      try AVAudioSession.sharedInstance().setActive(true)
+    } catch let error as NSError {
+      print(error)
+    }
+  }
+  
   
   override func viewDidLoad() {
     super.viewDidLoad()
     
+    changeAudioMode()
+    maxCount = Int((1/updateInterval) * codeWindow)
+    //print("maxcount: \(maxCount)")
+    
     AKSettings.audioInputEnabled = true
     AKSettings.sampleRate = 96000
     mic = AKMicrophone()
-    tracker = AKFrequencyTracker.init(mic, hopSize:64, peakCount:100)
+    tracker = AKFrequencyTracker.init(mic, hopSize:32, peakCount:2)
     silence = AKBooster(tracker, gain: 0)
 //    fft = AKFFTTap(mic)
     
@@ -51,38 +82,89 @@ class ViewController: UIViewController {
     NotificationCenter.default.removeObserver(self, name: NSNotification.Name.AVPlayerItemDidPlayToEndTime, object: nil)
   }
   
+  func isMatch(input:Double,reference:Double) -> Bool {
+    if (abs(reference - input) < frequencyRange) {
+      return true
+    }
+    return false
+  }
+  
+  func resetListenState() {
+    listenForCode = false
+    //listenTimer?.invalidate()
+    //listenTimer = nil;
+    counter = 0
+    print("Stop Listen")
+  }
+  
   func update() {
-    
-    
-    if tracker.frequency > 12000 {
-      //frequencyLabel.text = String(format: "%0.1f", tracker.frequency)
-      print(tracker.frequency)
-    
-      if tracker.frequency > 17900 {
-        colorBox.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1)
-      } else if tracker.frequency > 17400 {
-        colorBox.backgroundColor = UIColor(red: 0, green: 1, blue: 0, alpha: 1)
-      } else if tracker.frequency > 16900 {
-        colorBox.backgroundColor = UIColor(red: 0, green: 0, blue: 1, alpha: 1)
-        //setupVideo()
-      } else if tracker.frequency > 16400 {
-        colorBox.backgroundColor = UIColor(red: 1, green: 1, blue: 0, alpha: 1)
-      } else if tracker.frequency > 15900 {
-        colorBox.backgroundColor = UIColor(red: 0, green: 1, blue: 1, alpha: 1)
-      } else if tracker.frequency > 15400 {
-        colorBox.backgroundColor = UIColor(red: 1, green: 0, blue: 1, alpha: 1)
-      } else if tracker.frequency > 14900 {
-        colorBox.backgroundColor = UIColor(red: 1, green: 0, blue: 0, alpha: 1)
+    if listenForCode {
+      if counter < maxCount {
+        counter += 1
+      } else {
+        resetListenState()
       }
+    }
+    if tracker.frequency > 14000 {
+      print(tracker.frequency);
+      
+      if isMatch(input: tracker.frequency, reference: startSignal) {
+        print("Start Listen")
+        listenForCode = true;
+        counter = 0
+        //listenTimer?.invalidate()
+        //listenTimer = Timer.scheduledTimer(timeInterval: codeWindow, target: self, selector: #selector(ViewController.resetListenState), userInfo: nil, repeats: false)
+        
+      } else if listenForCode {
+        for option in signalOptions {
+          if isMatch(input: tracker.frequency, reference: option.freq) {
+            print("Match. Will load: \(option.file)")
+            resetListenState()
+            if let name = getCurrentVideoName() {
+              if name == option.file {
+                print("Already Loaded")
+                playCurrentVideo()
+              } else {
+                cueAndPlayVideo(fileName: option.file)
+              }
+            }
+            break
+          }
+        }
+        
+      }
+      
+      
+      
+//      //frequencyLabel.text = String(format: "%0.1f", tracker.frequency)
+//      print(tracker.frequency)
+//    
+//      if tracker.frequency > 17900 {
+//        colorBox.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1)
+//      } else if tracker.frequency > 17400 {
+//        colorBox.backgroundColor = UIColor(red: 0, green: 1, blue: 0, alpha: 1)
+//      } else if tracker.frequency > 16900 {
+//        colorBox.backgroundColor = UIColor(red: 0, green: 0, blue: 1, alpha: 1)
+//        //setupVideo()
+//      } else if tracker.frequency > 16400 {
+//        colorBox.backgroundColor = UIColor(red: 1, green: 1, blue: 0, alpha: 1)
+//      } else if tracker.frequency > 15900 {
+//        colorBox.backgroundColor = UIColor(red: 0, green: 1, blue: 1, alpha: 1)
+//      } else if tracker.frequency > 15400 {
+//        colorBox.backgroundColor = UIColor(red: 1, green: 0, blue: 1, alpha: 1)
+//      } else if tracker.frequency > 14900 {
+//        colorBox.backgroundColor = UIColor(red: 1, green: 0, blue: 0, alpha: 1)
+//      }
     }
   }
   
   func setupVideo() {
     
-    let pathToEx1 = Bundle.main.path(forResource: "Videos/gwcTest", ofType: "mov")
+    let pathToEx1 = Bundle.main.path(forResource: "Videos/plantTest", ofType: "mp4")
     let pathURL = NSURL.fileURL(withPath: pathToEx1!)
     
     movPlayer = AVPlayer(url:pathURL)
+  
     let movLayer = AVPlayerLayer(player: movPlayer)
     vidContainer.layer.addSublayer(movLayer)
     movLayer.frame = vidContainer.bounds
@@ -110,11 +192,13 @@ class ViewController: UIViewController {
   func playCurrentVideo() {
     vidContainer.alpha = 1.0;
     movPlayer?.play()
+    //changeAudioMode()
   }
   
   func stopVideo() {
     vidContainer.alpha = 0.0;
     movPlayer?.pause()
+    //changeAudioMode()
   }
   
   func playerDidFinishPlaying(note: Notification) {
@@ -136,7 +220,8 @@ class ViewController: UIViewController {
   }
   
   func cueAndPlayVideo(fileName: String) {
-    let pathToEx1 = Bundle.main.path(forResource: "Videos/\(fileName)", ofType: "mov")
+    //changeAudioMode()
+    let pathToEx1 = Bundle.main.path(forResource: "Videos/\(fileName)", ofType: "mp4")
     let pathURL = NSURL.fileURL(withPath: pathToEx1!)
     let asset = AVAsset(url: pathURL)
     //let assetKeys = [ "playable" ]
@@ -146,6 +231,7 @@ class ViewController: UIViewController {
     movPlayer?.volume = 0;
     vidContainer.alpha = 1.0;
     movPlayer?.play()
+    //AudioKit.start()
   }
 
   override func didReceiveMemoryWarning() {
